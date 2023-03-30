ca6fcb1452f4e9ca6e39f50413191e7ec5e278ec
Parth Paghdal <116045270+parth-paghdal-132@users.noreply.github.com>
2023-03-29 22:34:24 -0400

    firebase auth setup

---
+++ src/firebase/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+let firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
