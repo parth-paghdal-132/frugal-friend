df798dfc658dde56cd81360d2ea06e6eac88bdbb
Parth Paghdal <116045270+parth-paghdal-132@users.noreply.github.com>
2023-04-27 22:56:20 -0400

    merge issue resolved

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,4 +1,8 @@
 const redis = require("redis");
-const client = redis.createClient();
+const ck = require("ckey")
+
+const client = redis.createClient({
+    url: ck.REDIS_URL
+});
 
 module.exports = client;
